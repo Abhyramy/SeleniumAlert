0a62e76700f981bd3a6c5fb35963c1a7ba524e31
Abhyramy <abhykaran007@gmail.com>
2021-12-23 13:42:54 +0530

    Selenium Alerts

---
+++ src/org/alerts/Alerts.java
@@ -0,0 +1,32 @@
+package org.alerts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Abhyramy\\Desktop\\ABHY - Java Selenium\\Java\\SeleniumAlerts\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Alerts.html");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.xpath("//a[text()='Alert with OK & Cancel ']")).click();
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+
+		// switching alert:
+		Alert b = driver.switchTo().alert();
+
+		// to get the existing text:
+		System.out.println("\n\t" + b.getText());
+
+		Thread.sleep(3000);
+		// dismissing alert:
+		b.dismiss();
+
+	}
+}
